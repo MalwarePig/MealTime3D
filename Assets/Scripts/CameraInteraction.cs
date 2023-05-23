9357a21be7705adf18c197d27a50eb9e307bd43d
Sergio Magaña <sergio_lcc92@hotmail.com>
2023-05-23 17:19:29 -0600

    Tomar elementos

--- Assets/Scripts/CameraInteraction.cs
+++ Assets/Scripts/CameraInteraction.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class CameraInteraction : MonoBehaviour
 {
+    public Transform ojos;
     private new Transform camera;
-    [SerializeField] private float rayDistance;
+
+    [SerializeField]
+    private float rayDistance;
+
+    [SerializeField]
+    private Transform ObjetoEnMano;
+
+    private bool Ocupado = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +26,34 @@ public class CameraInteraction : MonoBehaviour
     {
         Debug.DrawRay(camera.position, camera.forward * rayDistance, Color.red);
         RaycastHit hit;
-        if (Physics.Raycast(camera.position, camera.forward, out hit, rayDistance, LayerMask.GetMask("Interactable") )){
+        if (
+            Physics.Raycast(
+                camera.position,
+                camera.forward,
+                out hit,
+                rayDistance,
+                LayerMask.GetMask("Interactable")
+            )
+        )
+        {
             Debug.Log(hit.transform.name);
-        }
+            if (Input.GetKeyDown(KeyCode.E) && Ocupado == false)
+            {
+                Debug.Log("Chi");
+                ObjetoEnMano = hit.transform;
+                ObjetoEnMano.SetParent(ojos); //Se adhiere a padre (manos)
+                ObjetoEnMano.position = ojos.position; //se coloca en zona de agarre (manos)
+                ObjetoEnMano.GetComponent<Rigidbody>().useGravity = false; //Se retiran las fisicas
+                ObjetoEnMano.GetComponent<Rigidbody>().isKinematic = true;
+                Ocupado = true; //Objeto en mano
+            }if (Input.GetKeyDown(KeyCode.Q) && Ocupado == true)
+            {
+                Debug.Log("Ño");
+                Ocupado = false;
+                ObjetoEnMano.SetParent(null); //Se adhiere a padre (manos)
+                ObjetoEnMano.GetComponent<Rigidbody>().useGravity = true; //Se retiran las fisicas
+                ObjetoEnMano.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }  
     }
 }
